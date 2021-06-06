f58fc5c899193af04d0c05e639d7294d5e767410
tbang-350 <59138492+tbang-350@users.noreply.github.com>
2021-06-06 16:34:58 +0300

    figured out the logic

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -2,32 +2,35 @@ package com.example.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView NumDisp;
-    private int counter;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NumDisp = (TextView) findViewById(R.id.NumDisp);
     }
 
-    public void AddbyOne(View view) {
-        counter = 0;
-        int disp = counter ++;
 
-        NumDisp.setText(String.valueOf(disp));
+    public void AddbyOne(View view) {
+        //counter = 0;
+        counter++;
+
+        NumDisp.setText(Integer.toString(counter));
 
     }
 
     public void DecreasebyOne(View view) {
-        counter = 0;
-        int disp = counter --;
+        //counter = 0;
+        counter--;
 
-        NumDisp.setText(String.valueOf(disp));
+        NumDisp.setText(Integer.toString(counter));
     }
 }
