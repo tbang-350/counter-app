146166d2b453061125fe6bad642c14404ea7a1ce
tbang-350 <59138492+tbang-350@users.noreply.github.com>
2021-06-06 18:28:16 +0300

    refactor textview id

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -7,25 +7,25 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView NumDisp;
+    private TextView Display;
     private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NumDisp = (TextView) findViewById(R.id.NumDisp);
+        Display = (TextView) findViewById(R.id.NumDisp);
     }
 
 
     public void AddbyOne(View view) {
         count++;
-        NumDisp.setText(Integer.toString(count));
+        Display.setText(Integer.toString(count));
 
     }
 
     public void DecreasebyOne(View view) {
         count--;
-        NumDisp.setText(Integer.toString(count));
+        Display.setText(Integer.toString(count));
     }
 }
